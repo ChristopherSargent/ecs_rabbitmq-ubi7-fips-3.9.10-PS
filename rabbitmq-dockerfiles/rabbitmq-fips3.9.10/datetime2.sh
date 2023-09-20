bc3e6d7c75f3957a57245542e62666827eb39bf1
Chris Sargent <chris.sargent-CTR@ecstech.com>
2023-09-20 17:47:19 +0000

    Updated directories and rabbitmq-ubi7-fips:3.9.10 Dockerfile to fix rabbitmqadmin and add python3.6 CAS 09202023

---
+++ rabbitmq-dockerfiles/rabbitmq-fips3.9.10/datetime2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Christopher Sargent 09202023
+set -x #echo on
+
+# Add date time stamp to root and jboss prompts/history's
+echo "export PROMPT_COMMAND='echo -n \[\$(date +%F-%T)\]\ '" >> /root/.bashrc && echo "export HISTTIMEFORMAT='%F-%T '" >> /root/.bashrc && source /root/.bashrc
+echo "source scl_source enable rh-python36" >> /root/.bashrc && source /root/.bashrc
+
+# Add ll alias 
+echo "alias ll='ls -alF'" >> /root/.bashrc && source /root/.bashrc
